35f3bdaf124a4d1796cb9d44eff1351fe6088f9e
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-15 11:00:05 -0800

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
 import './App.css';
-// import { useEffect, useState } from 'react';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import Characters from './views/Characters/Characters';
 import Films from './views/Films/Films';
 import Books from './views/Books/Books';
 
 function App() {
-  //   const [films, setFilms] = useState([]);
-  //   const [characters, setCharacters] = useState([]);
-  //   const [books, setBooks] = useState([]);
-
-  // useEffect(() => {
-  //   getFilms();
-  //   getCharacters();
-  //   // getBooks();
-  // }, []);
-
-  // const getFilms = async () => {
-  //   const response = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/films`, {
-  //     headers: {
-  //       apikey: process.env.REACT_APP_SUPABASE_KEY,
-  //       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-  //     },
-  //   });
-
-  //   const data = await response.json();
-  //   const filmData = data.map((film) => [
-  //     film.item,
-  //     film.title.toLowerCase().replace(/ /g, '-'),
-  //     film.box_office_total,
-  //     film.academy_award_nominations,
-  //   ]);
-  //   setFilms(filmData);
-  //   return [FilmList];
-  // };
-
-  // const getCharacters = async () => {
-  //   const response = await fetch(`${process.env.REACT_APP_SUPABASE_KEY}/rest/v1/characters`, {
-  //     headers: {
-  //       apikey: process.env.REACT_APP_SUPABASE_KEY,
-  //       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-  //     },
-  //   });
-  //   const data = await response.json();
-  //   const characterData = data.map((character) => ({
-  //     name: character.name,
-  //     birth: character.birth,
-  //     death: character.death,
-  //     dates:
-  //       character.dates === character.death ? 'Unknown' : `${character.birth} - ${character.death}`,
-  //   }));
-
-  //   setCharacters(characterData);
-  //   return [CharactersList];
-  // };
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -72,7 +22,7 @@ function App() {
             Books
           </NavLink>
         </header>
-        <Switch>
+        <Switch className="links">
           <Route exact path="/"></Route>
           <Route path="/films">
             <Films />
